131d5857afbeec23b14bd633503efb2eb6fe6af3
Eugene Volkov <eugenewolf507@gmail.com>
2020-10-24 20:25:55 +0300

    resolved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let abs = Math.abs(n);
+    let str = String(abs);
+    let revStr = str.split("").reverse().join("");
+    return Number.parseInt(revStr);
+};
